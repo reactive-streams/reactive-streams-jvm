9c3bb17badc3bdbf347d1e076001267bd758fb88
Viktor Klang <viktor.klang@gmail.com>
2014-05-26 17:13:00 +0200

    Fixes (re-adds) the processor interface as per consensus in Issue #22

---
+++ api/src/main/java/org/reactivestreams/Processor.java
@@ -0,0 +1,8 @@
+package org.reactivestreams;
+
+/**
+ * A Processor represents a processing stage—which is both a {@link Subscriber}
+ * and a {@link Publisher} and obeys the contracts of both.
+ */
+public interface Processor<T, R> extends Subscriber<T>, Publisher<R> {
+}
