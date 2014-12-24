e1cb0cb1bdb0ed28c5273650394469fee9bbc60b
Viktor Klang <viktor.klang@gmail.com>
2014-12-24 20:01:08 +0100

    Removes NumberIterablePublisher's type parameter and makes all the example implementations public

--- examples/src/main/java/org/reactivestreams/example/unicast/AsyncIterablePublisher.java
+++ examples/src/main/java/org/reactivestreams/example/unicast/AsyncIterablePublisher.java
@@ -14,10 +14,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * AsyncIterablePublisher is an implementation of Reactive Streams `Publisher`
  * which executes asynchronously, using a provided `Executor` and produces elements
  * from a given `Iterable` in a "unicast" configuration to its `Subscribers`.
- 
+ *
  * NOTE: The code below uses a lot of try-catches to show the reader where exceptions can be expected, and where they are forbidden.
  */
-class AsyncIterablePublisher<T> implements Publisher<T> {
+public class AsyncIterablePublisher<T> implements Publisher<T> {
   private final static int DEFAULT_BATCHSIZE = 1024;
 
   private final Iterable<T> elements; // This is our data source / generator
