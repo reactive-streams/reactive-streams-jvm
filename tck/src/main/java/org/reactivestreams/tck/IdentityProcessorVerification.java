joobn72/reactive-streams

Clarifies the signalling sequence in the spec and  adds TCK verification to ensure signal ordering is proper,  also amends the examples to reflect the spec change.

tck/src/main/java/org/reactivestreams/tck/IdentityProcessorVerification.java
@@ -287,6 +287,11 @@ public abstract class IdentityProcessorVerification<T> extends WithHelperPublish
   }
 
   @Override @Test
+  public void required_spec112_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable {
+    publisherVerification.required_spec112_mustIssueOnSubscribeForNonNullSubscriber();
+  }
+
+  @Override @Test
   public void required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable {
     publisherVerification.required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
   }

tck/src/main/java/org/reactivestreams/tck/PublisherVerification.java
@@ -14,6 +14,7 @@ import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.lang.Override;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -344,16 +345,24 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
       whenHasErrorPublisherTest(new PublisherTestRun<T>() {
         @Override
         public void run(final Publisher<T> pub) throws InterruptedException {
-          final Latch latch = new Latch(env);
+          final Latch onErrorlatch = new Latch(env);
+          final Latch onSubscribeLatch = new Latch(env);
           pub.subscribe(new TestEnvironment.TestSubscriber<T>(env) {
             @Override
+            public void onSubscribe(Subscription subs) {
+              onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+              onSubscribeLatch.close();
+            }
+            @Override
             public void onError(Throwable cause) {
-              latch.assertOpen(String.format("Error-state Publisher %s called `onError` twice on new Subscriber", pub));
-              latch.close();
+              onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
+              onErrorlatch.assertOpen(String.format("Error-state Publisher %s called `onError` twice on new Subscriber", pub));
+              onErrorlatch.close();
             }
           });
 
-          latch.expectClose(String.format("Error-state Publisher %s did not call `onError` on new Subscriber", pub));
+          onSubscribeLatch.expectClose("Should have received onSubscribe");
+          onErrorlatch.expectClose(String.format("Error-state Publisher %s did not call `onError` on new Subscriber", pub));
 
           env.verifyNoAsyncErrors(env.defaultTimeoutMillis());
           }
@@ -432,6 +441,7 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
     notVerified(); // can we meaningfully test this?
   }
 
+  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
   @Override @Test
   public void untested_spec109_subscribeShouldNotThrowNonFatalThrowable() throws Throwable {
     notVerified(); // can we meaningfully test this?
@@ -440,16 +450,16 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
   // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
   @Override @Test
   public void required_spec109_subscribeThrowNPEOnNullSubscriber() throws Throwable {
-      activePublisherTest(0, false, new PublisherTestRun<T>() {
-        @Override
-        public void run(Publisher<T> pub) throws Throwable {
-            try {
-                pub.subscribe(null);
-                env.flop(String.format("Publisher (%s) did not throw a NullPointerException when given a null Subscribe in subscribe", pub));
-            } catch (NullPointerException npe) {
-            }
-            env.verifyNoAsyncErrors();
+    activePublisherTest(0, false, new PublisherTestRun<T>() {
+      @Override
+      public void run(Publisher<T> pub) throws Throwable {
+        try {
+            pub.subscribe(null);
+            env.flop("Publisher did not throw a NullPointerException when given a null Subscribe in subscribe");
+        } catch (NullPointerException npe) {
         }
+        env.verifyNoAsyncErrors();
+      }
     });
   }
 
@@ -475,25 +485,64 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
 
   // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.12
   @Override @Test
+  public void required_spec112_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable {
+    activePublisherTest(0, false, new PublisherTestRun<T>() {
+      @Override
+      public void run(Publisher<T> pub) throws Throwable {
+        final Latch onSubscribeLatch = new Latch(env);
+        pub.subscribe(new Subscriber<T>() {
+          @Override
+          public void onError(Throwable cause) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
+          }
+
+          @Override
+          public void onSubscribe(Subscription subs) {
+            onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+            onSubscribeLatch.close();
+          }
+
+          @Override
+          public void onNext(T elem) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onNext always");
+          }
+
+          @Override
+          public void onComplete() {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onComplete always");
+          }
+        });
+        onSubscribeLatch.expectClose("Should have received onSubscribe");
+        env.verifyNoAsyncErrors();
+      }
+    });
+  }
+
+  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.12
+  @Override @Test
   public void required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable {
     whenHasErrorPublisherTest(new PublisherTestRun<T>() {
       @Override
       public void run(Publisher<T> pub) throws Throwable {
         final Latch onErrorLatch = new Latch(env);
+        final Latch onSubscribeLatch = new Latch(env);
         ManualSubscriberWithSubscriptionSupport<T> sub = new ManualSubscriberWithSubscriptionSupport<T>(env) {
           @Override
           public void onError(Throwable cause) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
             onErrorLatch.assertOpen("Only one onError call expected");
             onErrorLatch.close();
           }
 
           @Override
           public void onSubscribe(Subscription subs) {
-            env.flop("onSubscribe should not be called if Publisher is unable to subscribe a Subscriber");
+            onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+            onSubscribeLatch.close();
           }
         };
         pub.subscribe(sub);
-        onErrorLatch.assertClosed("Should have received onError");
+        onSubscribeLatch.expectClose("Should have received onSubscribe");
+        onErrorLatch.expectClose("Should have received onError");
 
         env.verifyNoAsyncErrors();
         }

tck/src/main/java/org/reactivestreams/tck/support/PublisherVerificationRules.java
@@ -24,6 +24,7 @@ public interface PublisherVerificationRules {
   void required_spec109_subscribeThrowNPEOnNullSubscriber() throws Throwable;
   void untested_spec110_rejectASubscriptionRequestIfTheSameSubscriberSubscribesTwice() throws Throwable;
   void optional_spec111_maySupportMultiSubscribe() throws Throwable;
+  void required_spec112_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable;
   void required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable;
   void required_spec113_mustProduceTheSameElementsInTheSameSequenceToAllOfItsSubscribersWhenRequestingOneByOne() throws Throwable;
   void required_spec113_mustProduceTheSameElementsInTheSameSequenceToAllOfItsSubscribersWhenRequestingManyUpfront() throws Throwable;

tck/src/test/java/org/reactivestreams/tck/PublisherVerificationTest.java
@@ -251,6 +251,19 @@ public class PublisherVerificationTest extends TCKVerificationSupport {
   }
 
   @Test
+  public void required_spec109_subscribeThrowNPEOnNullSubscriber_shouldFailIfDoesntThrowNPE() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(final Subscriber<? super Integer> s) {
+
+          }
+        }).required_spec109_subscribeThrowNPEOnNullSubscriber();
+      }
+    }, "Publisher did not throw a NullPointerException when given a null Subscribe in subscribe");
+  }
+
+  @Test
   public void untested_spec110_rejectASubscriptionRequestIfTheSameSubscriberSubscribesTwice_shouldFailBy_skippingSinceOptional() throws Throwable {
     requireTestFailure(new ThrowingRunnable() {
       @Override public void run() throws Throwable {
@@ -265,11 +278,51 @@ public class PublisherVerificationTest extends TCKVerificationSupport {
   }
 
   @Test
+  public void required_spec112_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnCompleteHappensFirst() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onComplete();
+          }
+        }).required_spec112_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onComplete always");
+  }
+
+  @Test
+  public void required_spec112_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnNextHappensFirst() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onNext(1337);
+          }
+        }).required_spec112_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onNext always");
+  }
+
+  @Test
+  public void required_spec112_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnErrorHappensFirst() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onError(new TestException());
+          }
+        }).required_spec112_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onError always");
+  }
+
+  @Test
   public void optional_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe_shouldFail() throws Throwable {
     requireTestFailure(new ThrowingRunnable() {
       @Override public void run() throws Throwable {
         customPublisherVerification(SKIP, new Publisher<Integer>() {
           @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onSubscribe(new NoopSubscription());
           }
         }).required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
       }
@@ -280,6 +333,7 @@ public class PublisherVerificationTest extends TCKVerificationSupport {
   public void optional_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe_actuallyPass() throws Throwable {
     customPublisherVerification(SKIP, new Publisher<Integer>() {
       @Override public void subscribe(Subscriber<? super Integer> s) {
+        s.onSubscribe(new NoopSubscription());
         s.onError(new RuntimeException("Sorry, I'm busy now. Call me later."));
       }
     }).required_spec112_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();

reactive-streams/reactive-streams-jvm

tck/src/main/java/org/reactivestreams/tck/IdentityProcessorVerification.java
@@ -277,8 +277,13 @@ public abstract class IdentityProcessorVerification<T> extends WithHelperPublish
   }
 
   @Override @Test
-  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable {
-    publisherVerification.required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
+  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe() throws Throwable {
+    publisherVerification.required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe();
+  }
+
+  @Override @Test
+  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable {
+    publisherVerification.required_spec109_mustIssueOnSubscribeForNonNullSubscriber();
   }
 
   @Override @Test

tck/src/main/java/org/reactivestreams/tck/PublisherVerification.java
@@ -14,6 +14,7 @@ import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.lang.Override;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -344,16 +345,24 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
       whenHasErrorPublisherTest(new PublisherTestRun<T>() {
         @Override
         public void run(final Publisher<T> pub) throws InterruptedException {
-          final Latch latch = new Latch(env);
+          final Latch onErrorlatch = new Latch(env);
+          final Latch onSubscribeLatch = new Latch(env);
           pub.subscribe(new TestEnvironment.TestSubscriber<T>(env) {
             @Override
+            public void onSubscribe(Subscription subs) {
+              onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+              onSubscribeLatch.close();
+            }
+            @Override
             public void onError(Throwable cause) {
-              latch.assertOpen(String.format("Error-state Publisher %s called `onError` twice on new Subscriber", pub));
-              latch.close();
+              onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
+              onErrorlatch.assertOpen(String.format("Error-state Publisher %s called `onError` twice on new Subscriber", pub));
+              onErrorlatch.close();
             }
           });
 
-          latch.expectClose(String.format("Error-state Publisher %s did not call `onError` on new Subscriber", pub));
+          onSubscribeLatch.expectClose("Should have received onSubscribe");
+          onErrorlatch.expectClose(String.format("Error-state Publisher %s did not call `onError` on new Subscriber", pub));
 
           env.verifyNoAsyncErrors(env.defaultTimeoutMillis());
           }
@@ -432,6 +441,7 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
     notVerified(); // can we meaningfully test this?
   }
 
+  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
   @Override @Test
   public void untested_spec109_subscribeShouldNotThrowNonFatalThrowable() throws Throwable {
     notVerified(); // can we meaningfully test this?
@@ -440,40 +450,79 @@ public abstract class PublisherVerification<T> implements PublisherVerificationR
   // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
   @Override @Test
   public void required_spec109_subscribeThrowNPEOnNullSubscriber() throws Throwable {
-      activePublisherTest(0, false, new PublisherTestRun<T>() {
-        @Override
-        public void run(Publisher<T> pub) throws Throwable {
-            try {
-                pub.subscribe(null);
-                env.flop(String.format("Publisher (%s) did not throw a NullPointerException when given a null Subscribe in subscribe", pub));
-            } catch (NullPointerException npe) {
-            }
-            env.verifyNoAsyncErrors();
+    activePublisherTest(0, false, new PublisherTestRun<T>() {
+      @Override
+      public void run(Publisher<T> pub) throws Throwable {
+        try {
+            pub.subscribe(null);
+            env.flop("Publisher did not throw a NullPointerException when given a null Subscribe in subscribe");
+        } catch (NullPointerException npe) {
         }
+        env.verifyNoAsyncErrors();
+      }
     });
   }
 
-  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.09
+  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
   @Override @Test
-  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable {
+  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable {
+    activePublisherTest(0, false, new PublisherTestRun<T>() {
+      @Override
+      public void run(Publisher<T> pub) throws Throwable {
+        final Latch onSubscribeLatch = new Latch(env);
+        pub.subscribe(new Subscriber<T>() {
+          @Override
+          public void onError(Throwable cause) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
+          }
+
+          @Override
+          public void onSubscribe(Subscription subs) {
+            onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+            onSubscribeLatch.close();
+          }
+
+          @Override
+          public void onNext(T elem) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onNext always");
+          }
+
+          @Override
+          public void onComplete() {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onComplete always");
+          }
+        });
+        onSubscribeLatch.expectClose("Should have received onSubscribe");
+        env.verifyNoAsyncErrors();
+      }
+    });
+  }
+
+  // Verifies rule: https://github.com/reactive-streams/reactive-streams#1.9
+  @Override @Test
+  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe() throws Throwable {
     whenHasErrorPublisherTest(new PublisherTestRun<T>() {
       @Override
       public void run(Publisher<T> pub) throws Throwable {
         final Latch onErrorLatch = new Latch(env);
+        final Latch onSubscribeLatch = new Latch(env);
         ManualSubscriberWithSubscriptionSupport<T> sub = new ManualSubscriberWithSubscriptionSupport<T>(env) {
           @Override
           public void onError(Throwable cause) {
+            onSubscribeLatch.assertClosed("onSubscribe should be called prior to onError always");
             onErrorLatch.assertOpen("Only one onError call expected");
             onErrorLatch.close();
           }
 
           @Override
           public void onSubscribe(Subscription subs) {
-            env.flop("onSubscribe should not be called if Publisher is unable to subscribe a Subscriber");
+            onSubscribeLatch.assertOpen("Only one onSubscribe call expected");
+            onSubscribeLatch.close();
           }
         };
         pub.subscribe(sub);
-        onErrorLatch.assertClosed("Should have received onError");
+        onSubscribeLatch.expectClose("Should have received onSubscribe");
+        onErrorLatch.expectClose("Should have received onError");
 
         env.verifyNoAsyncErrors();
       }

tck/src/main/java/org/reactivestreams/tck/support/PublisherVerificationRules.java
@@ -20,9 +20,10 @@ public interface PublisherVerificationRules {
   void required_spec107_mustNotEmitFurtherSignalsOnceOnCompleteHasBeenSignalled() throws Throwable;
   void untested_spec107_mustNotEmitFurtherSignalsOnceOnErrorHasBeenSignalled() throws Throwable;
   void untested_spec108_possiblyCanceledSubscriptionShouldNotReceiveOnErrorOrOnCompleteSignals() throws Throwable;
+  void required_spec109_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable;
   void untested_spec109_subscribeShouldNotThrowNonFatalThrowable() throws Throwable;
   void required_spec109_subscribeThrowNPEOnNullSubscriber() throws Throwable;
-  void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe() throws Throwable;
+  void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe() throws Throwable;
   void untested_spec110_rejectASubscriptionRequestIfTheSameSubscriberSubscribesTwice() throws Throwable;
   void optional_spec111_maySupportMultiSubscribe() throws Throwable;
   void required_spec112_mustProduceTheSameElementsInTheSameSequenceToAllOfItsSubscribersWhenRequestingOneByOne() throws Throwable;

tck/src/test/java/org/reactivestreams/tck/PublisherVerificationTest.java
@@ -251,31 +251,88 @@ public class PublisherVerificationTest extends TCKVerificationSupport {
   }
 
   @Test
-  public void optional_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe_actuallyPass() throws Throwable {
-    customPublisherVerification(SKIP, new Publisher<Integer>() {
-      @Override public void subscribe(Subscriber<? super Integer> s) {
-        s.onError(new RuntimeException("Sorry, I'm busy now. Call me later."));
+  public void required_spec109_subscribeThrowNPEOnNullSubscriber_shouldFailIfDoesntThrowNPE() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(final Subscriber<? super Integer> s) {
+
+          }
+        }).required_spec109_subscribeThrowNPEOnNullSubscriber();
       }
-    }).required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
+    }, "Publisher did not throw a NullPointerException when given a null Subscribe in subscribe");
   }
 
   @Test
-  public void optional_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe_shouldFail() throws Throwable {
+  public void optional_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe_actuallyPass() throws Throwable {
+    customPublisherVerification(SKIP, new Publisher<Integer>() {
+      @Override public void subscribe(Subscriber<? super Integer> s) {
+        s.onSubscribe(new NoopSubscription());
+        s.onError(new RuntimeException("Sorry, I'm busy now. Call me later."));
+      }
+    }).required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe();
+  }
+
+  @Test
+  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnCompleteHappensFirst() throws Throwable {
     requireTestFailure(new ThrowingRunnable() {
       @Override public void run() throws Throwable {
-        customPublisherVerification(SKIP, new Publisher<Integer>() {
-          @Override public void subscribe(Subscriber<? super Integer> s) {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override
+          public void subscribe(Subscriber<? super Integer> s) {
+            s.onComplete();
           }
-        }).required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
+        }).required_spec109_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onComplete always");
+  }
+
+  @Test
+  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnNextHappensFirst() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onNext(1337);
+          }
+        }).required_spec109_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onNext always");
+  }
+
+  @Test
+  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber_mustFailIfOnErrorHappensFirst() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override public void run() throws Throwable {
+        customPublisherVerification(new Publisher<Integer>() {
+          @Override public void subscribe(Subscriber<? super Integer> s) {
+            s.onError(new TestException());
+          }
+        }).required_spec109_mustIssueOnSubscribeForNonNullSubscriber();
+      }
+    }, "onSubscribe should be called prior to onError always");
+  }
+
+  @Test
+  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe_shouldFail() throws Throwable {
+    requireTestFailure(new ThrowingRunnable() {
+      @Override
+      public void run() throws Throwable {
+        customPublisherVerification(SKIP, new Publisher<Integer>() {
+          @Override
+          public void subscribe(Subscriber<? super Integer> s) {
+            s.onSubscribe(new NoopSubscription());
+          }
+        }).required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe();
       }
     }, "Should have received onError");
   }
 
   @Test
-  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe_beSkippedForNoGivenErrorPublisher() throws Throwable {
+  public void required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe_beSkippedForNoGivenErrorPublisher() throws Throwable {
     requireTestSkip(new ThrowingRunnable() {
       @Override public void run() throws Throwable {
-        noopPublisherVerification().required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorInsteadOfOnSubscribe();
+        noopPublisherVerification().required_spec109_mayRejectCallsToSubscribeIfPublisherIsUnableOrUnwillingToServeThemRejectionMustTriggerOnErrorAfterOnSubscribe();
       }
     }, PublisherVerification.SKIPPING_NO_ERROR_PUBLISHER_AVAILABLE);
   }