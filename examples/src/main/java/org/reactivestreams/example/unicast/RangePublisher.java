e7eaa6b7c96b64c92032d2699e350aafd409c6e5
akarnokd <akarnokd@gmail.com>
2017-10-03 14:43:09 +0200

    Move classes to the unicast package.

--- examples/src/main/java/org/reactivestreams/example/unicast/RangePublisher.java
+++ examples/src/main/java/org/reactivestreams/example/unicast/RangePublisher.java
@@ -9,7 +9,7 @@
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.*
  ************************************************************************/
 
-package org.reactivestreams.example.lazycast;
+package org.reactivestreams.example.unicast;
 
 import org.reactivestreams.*;
 
