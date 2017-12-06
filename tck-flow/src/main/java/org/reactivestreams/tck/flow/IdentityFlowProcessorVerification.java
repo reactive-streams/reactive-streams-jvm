872a7ea998bc5102a1658ccb1fd70158a4eab2d2
akarnokd <akarnokd@gmail.com>
2017-12-06 14:43:39 +0100

    Replicate Javadoc from the bridged original methods

--- tck-flow/src/main/java/org/reactivestreams/tck/flow/IdentityFlowProcessorVerification.java
+++ tck-flow/src/main/java/org/reactivestreams/tck/flow/IdentityFlowProcessorVerification.java
@@ -34,8 +34,23 @@ public abstract class IdentityFlowProcessorVerification<T> extends IdentityProce
     super(env, publisherReferenceGCTimeoutMillis, processorBufferSize);
   }
 
+  /**
+   * By implementing this method, additional TCK tests concerning a "failed" Flow publishers will be run.
+   *
+   * The expected behaviour of the {@link Flow.Publisher} returned by this method is hand out a subscription,
+   * followed by signalling {@code onError} on it, as specified by Rule 1.9.
+   *
+   * If you ignore these additional tests, return {@code null} from this method.
+   */
   protected abstract Flow.Publisher<T> createFailedFlowPublisher();
 
+  /**
+   * This is the main method you must implement in your test incarnation.
+   * It must create a {@link Flow.Publisher}, which simply forwards all stream elements from its upstream
+   * to its downstream. It must be able to internally buffer the given number of elements.
+   *
+   * @param bufferSize number of elements the processor is required to be able to buffer.
+   */
   protected abstract Flow.Processor<T,T> createIdentityFlowProcessor(int bufferSize);
 
   @Override
